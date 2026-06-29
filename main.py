5c6254173f2d7bba5517bcae2c261f730ad72002
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2026-06-29 19:33:08 +0200

    modelos y main

--- main.py
+++ main.py
@@ -1,1 +1,73 @@
-print("HELLO!!")
+import sys
+from .models import Drone_Map, Hub, Connection
+
+
+
+class  Parser:
+    def __init__(self, drone_map:Drone_Map) -> None:
+        self.map:Drone_Map = drone_map
+        self.nb_drones: int = 0
+
+    def parse_file(self, map_path:str) -> None:
+        try:
+            with open(map_path, "r") as file:
+                for line_num, line, in enumerate(file, 1):
+                    clean_line = line.strip()
+
+                    if not clean_line or clean_line.startswith("#"):
+                        continue
+                    self.parse_line(clean_line, line_num)
+        
+        except FileNotFoundError:
+            print(f"Error: El archivo '{file_path}' no existe.", file=sys.stderr)
+            sys.exit(1)
+
+    def parse_line(self, line:str, line_num:int) -> None:
+
+        if line.startswith("nb__drones:"):
+            self.nb_drones = int(line.split(":")[1].strip())
+
+        elif(
+            line.startswith("hub:") or line.startswith("start_hub:") \
+            or line.startswith("end_hub:")
+            ):
+
+            # TODO: Extraer coordenadas, nombre y metadatos de los corchetes
+            # nuevo_hub = Hub(name, x, y, zo_type)
+            # self.map.add_hub(nuevo_hub)
+            pass
+
+        elif line.startswith("connection:"):
+            # Separar las dos zonas (ej: hub1-hub2) y buscar los objetos Hub en self.map.hubs
+            # nueva_conex = Connection(zone1, zone2, capacity)
+            # self.map.add_connection(nueva_conex)
+            pass
+
+        else:
+            print(f"Error de sintaxis en línea {line_num}: Comando desconocido.", file=sys.stderr)
+            sys.exit(1)
+
+
+    
+
+
+def main() -> None:
+
+
+    if len(sys.argv) < 2:
+        print("Uso: python3 -m tu_modulo mapa.txt", file=sys.stderr)
+        sys.exit(1)
+
+
+    drone_map = Drone_Map()
+    parser = Parser(drone_map)
+    parser.parse_file(sys.argv[1])
+    print(f"Mapa cargado exitosamente. Drones totales: {parser.nb_drones}")
+
+
+
+
+
+
+if  __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,47 @@
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+
+class Hub(BaseModel):
+    def __init__(
+            self, name:str, x:int, y:int, zo_type:str = "normal"
+            ) -> None:
+        
+        self.name: str = name
+        self.x: int = x
+        self.y: int = y
+        self.type: str = zo_type
+
+
+class Connection:
+    def __init__(
+            self, name:str, zone1:Hub, zone2:Hub, capacity:int = 1
+            ) -> None:
+
+        self.name: str = name
+        self.zone1: Hub = Hub
+        self.zone2: Hub = Hub
+        self.capacity: int = capacity
+
+
+class Drone_Map:
+    def __init__(self) -> None:
+
+        self.hubs:Dict[str, Hub] = {}
+        self.connections:list[Connection] = []
+    
+    def add_hub(self, hub:Hub) -> None:
+        new_hub = Hub()
+
+        if new_hub.name not in self.hubs:
+            self.hubs[new_hub.name] = hub
+        else:
+            raise ValueError(f"Error: El Hub con nombre '{hub.name}' ya existe.")
+
+    def add_conection(self, connection:Connection) -> None:
+        new_conex = Connection()
+
+        if new_conex not in self.conections:
+            connection.add(new_conex)
+        else:
+            raise ValueError(f"Error: La conexion '{connection}' ya existe")
+
