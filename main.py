18af7f5bbeb786e5146289d3a0c1696c9bfd787b
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2026-06-30 18:47:48 +0200

    flake8 principio

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import re
 from models import Drone_Map, Hub, Connection
 
 
-class   Parser:
+class Parser:
     def __init__(self, drone_map: Drone_Map) -> None:
-        self.map:Drone_Map = drone_map
+        self.map: Drone_Map = drone_map
         self.nb_drones: int = 0
         self.hub_counter = 1
         self.connection_counter: int = 0
@@ -29,19 +29,19 @@ class   Parser:
 
 
 def parse_hub_content(self, content: str) -> tuple[str, int, int, str]:
-    
-        match_color = re.search(r"\[color=(\w+)\]", content)
-        color = match_color.group(1) if match_color else "white"
 
-        # quita el bloque [color=...] para quedarte solo con "nombre x y"
-        main_part = re.sub(r"\[.*?\]", "", content).strip()
-        parts = main_part.split()
+    match_color = re.search(r"\[color=(\w+)\]", content)
+    color = match_color.group(1) if match_color else "white"
 
-        if len(parts) != 3:
-            raise ValueError(f"Formato de hub inválido: '{content}'")
+    # quita el bloque [color=...] para quedarte solo con "nombre x y"
+    main_part = re.sub(r"\[.*?\]", "", content).strip()
+    parts = main_part.split()
 
-        name, x_str, y_str = parts
-        return name, int(x_str), int(y_str), color
+    if len(parts) != 3:
+        raise ValueError(f"Formato de hub inválido: '{content}'")
+
+    name, x_str, y_str = parts
+    return name, int(x_str), int(y_str), color
 
 
 def parse_line(self, line: str, line_num: int) -> None:
@@ -49,7 +49,7 @@ def parse_line(self, line: str, line_num: int) -> None:
         self.nb_drones = int(line.split(":")[1].strip())
 
     elif any(line.startswith(p) for p in ["hub:", "start_hub:", "end_hub:"]):
-        
+
         prefix, content = line.split(":", 1)
         content = content.strip()
 
@@ -65,9 +65,12 @@ def parse_line(self, line: str, line_num: int) -> None:
                 self.hub_counter += 1
 
             case _:
-                print(f"Error de sintaxis en línea {line_num}: prefijo de hub desconocido.", file=sys.stderr)
+                print(
+                    f"Error de sintaxis en línea {line_num}: "
+                    "prefijo de hub desconocido.", file=sys.stderr
+                    )
                 sys.exit(1)
-        
+
         try:
             self.map.add_hub(nuevo_hub)
 
@@ -76,7 +79,7 @@ def parse_line(self, line: str, line_num: int) -> None:
             sys.exit(1)
 
     elif line.startswith("connection:"):
-        
+
         _, content = line.split(":", 1)
         zone_1, zone_2 = content.split("-", 1)
         zone_1 = zone_1.strip()
@@ -86,21 +89,27 @@ def parse_line(self, line: str, line_num: int) -> None:
         second_hub = self.map.hubs.get(zone_2)
 
         if not first_hub or not second_hub:
-            print(f"Error en línea {line_num}: zona no encontrada en la conexión.", file=sys.stderr)
+            print(
+                f"Error en línea {line_num}: zona no encontrada "
+                "en la conexión.", file=sys.stderr
+                )
             sys.exit(1)
 
         self.connection_counter += 1
-        new_connection = Connection(f"Conn{self.connection_counter}", first_hub, second_hub)
+        new_connection = Connection(
+            f"Conn{self.connection_counter}", first_hub, second_hub
+            )
         self.map.add_connection(new_connection)
-        
 
     else:
-        print(f"Error de sintaxis en línea {line_num}: Comando desconocido.", file=sys.stderr)
+        print(
+            f"Error de sintaxis en línea {line_num}: "
+            "Comando desconocido.", file=sys.stderr
+            )
         sys.exit(1)
 
 
 def main() -> None:
-
 
     if len(sys.argv) < 2:
         print("Uso: python3 -m tu_modulo mapa.txt", file=sys.stderr)
@@ -112,5 +121,5 @@ def main() -> None:
     print(f"Mapa cargado exitosamente. Drones totales: {parser.nb_drones}")
 
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     main()
